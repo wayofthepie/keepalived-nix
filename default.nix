7f846baf203982ce5bdb7fe62a002da41b35e308
wayofthepie <wayofthepie@gmail.com>
2023-06-09 18:43:50 +0100

    Initial commit

---
+++ default.nix
@@ -0,0 +1,39 @@
+{ version ? "0.0.0" }:
+
+let
+  pkgs = (import <nixpkgs> {}).pkgsCross.aarch64-multiplatform;
+  keepalived = with pkgs; stdenv.mkDerivation {
+    name = "keepalived";
+    src = fetchurl {
+      url = "https://www.keepalived.org/software/keepalived-2.2.8.tar.gz";
+      sha256 = "1dhvg9x976k4nnygxyv2gr55jfd88459kgiiqva9bwvl56v2x245";
+    };
+    buildInputs = [ pkgsStatic.openssl ];
+    preBuild = ''
+      makeFlagsArray=(
+                      CFLAGS="-static"
+                      LDFLAGS="-L${glibc.static}/lib")
+    '';
+    installPhase = ''
+      ./configure --disable-dynamic-linking
+      make
+      mkdir -p $out/bin
+      cp bin/keepalived $out/bin/keepalived
+      ${pkgs.upx}/bin/upx $out/bin/keepalived
+      ls -la $out/bin
+    '';
+  };
+
+  keepalivedImage = with pkgs; dockerTools.buildImage {
+    name = "wayofthepie/keepalived";
+    tag = "${version}";
+    copyToRoot = buildEnv {
+      name = "image-root";
+      paths = [ "${keepalived}" ];
+      pathsToLink = [ "/bin" ];
+    };
+    config = {
+      Cmd = [ "/bin/keepalived" ];
+    };
+  };
+in keepalivedImage
